0a04ddcd997b259e4a80543543f0e0d2fa23b50c
Bobettt <aleksandar.ostojic.bor@gmail.com>
2025-06-16 13:49:04 +0200

    Pomeranje mete 			# Please enter the commit message for your changes. Lines starting 		# with '#' will be ignored, and an empty message aborts the commit.

---
+++ physics.js
@@ -0,0 +1,25 @@
+let x = 0;
+let y = Math.random() * (maxDistance - minDistance) + minDistance;
+let speed = Math.random() * (max - min) + min;
+let minPosition = -1.0;
+let maxPosition = 1.0;
+let hitted = false;
+
+function update() {
+    if (!hitted) {
+        x += speed;
+
+        // promena pravca kad dotakne ivice
+        if (x > maxPosition || x < minPosition) {
+            speed = -speed;
+        }
+    }
+
+    // ovde šalješ poziciju u shader kao uniformu, npr.:
+    gl.uniform2f(positionUniformLocation, x, y);
+
+    drawScene();
+    requestAnimationFrame(update);
+}
+
+update();
